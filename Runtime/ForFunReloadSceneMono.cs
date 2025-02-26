0318c8f6eafc1c3617fbbdba0a8165faa733f46d
Eloi Stree <eloistree@gmail.com>
2025-02-26 23:44:44 +0100

    Save: 02/26/2025 11:44 PM

---
+++ Runtime/ForFunReloadSceneMono.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class ForFunReloadSceneMono : MonoBehaviour
+{
+
+    [ContextMenu("Reload current scene")]
+    public void ReloadCurrentScene() {
+
+      //  UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
